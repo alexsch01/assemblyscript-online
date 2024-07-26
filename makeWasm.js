28026abcba06f2143690c38349f2706e86cdf5c5
Alex Schwartz <alexschwartz01@gmail.com>
2024-07-26 16:17:43 -0400

    Update makeWasm.js

--- makeWasm.js
+++ makeWasm.js
@@ -1,21 +1,28 @@
 import asc from './assemblyscript/asc.js'
 
-globalThis.runClick = async function() {
+globalThis.print_WasmText = async function() {
     const { stderr, text, binary } = await asc.compileString(globalThis.codeEditor.getValue(), { optimize: 2 });
     const err = stderr.toString();
     if(err != "") {
         document.querySelector('#output').value = err;
-        return;
+        return -1;
     }
 
     document.querySelector('#output').value = text;
-    const blob = new Blob([binary], {type: 'application/octet-stream'});
-    const url = window.URL.createObjectURL(blob);
+    return binary;
+}
 
-    const a = document.createElement("a");
-    document.body.appendChild(a);
-    a.href = url;
-    a.download = "module.wasm";
-    a.click();
-    window.URL.revokeObjectURL(url);
+globalThis.download_WasmBinary = async function() {
+    const binary = await globalThis.print_WasmText()
+    if(binary != -1) {
+        const blob = new Blob([binary], {type: 'application/wasm'});
+        const url = window.URL.createObjectURL(blob);
+
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+        a.href = url;
+        a.download = "module.wasm";
+        a.click();
+        window.URL.revokeObjectURL(url);
+    }
 }
